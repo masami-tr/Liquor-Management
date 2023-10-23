545294354bdb631c1649d2cffe388099b7dddd00
masami-tr <m3o.mmmnnn@gmail.com>
2023-10-23 15:01:59 +0900

    　プロジェクトセットアップ

---
+++ src/main/java/com/raisetech10/liquor_management/LiquorManagementApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech10.liquor_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquorManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquorManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech10/liquor_management/LiquorManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech10.liquor_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquorManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
