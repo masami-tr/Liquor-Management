52b4a49048336d74afb219a21c55afe1c1061205
masami-tr <m3o.mmmnnn@gmail.com>
2023-11-13 14:20:28 +0900

    Mapper内のimportに関する設定変更

--- src/main/java/com/raisetech10/liquor_management/mapper/LiquorMapper.java
+++ src/main/java/com/raisetech10/liquor_management/mapper/LiquorMapper.java
@@ -1,7 +1,11 @@
 package com.raisetech10.liquor_management.mapper;
 
 import com.raisetech10.liquor_management.entity.Liquor;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 import java.util.Optional;
